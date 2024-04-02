f5c7925af90c2dc40287ca0985dbea534df8812e
datluu <tad.louis2810@gmail.com>
2024-04-02 15:45:26 +1100

    Initial Commit

---
+++ BubblePop/BubblePopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubblePopApp.swift
+//  BubblePop
+//
+//  Created by David on 2/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct BubblePopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
